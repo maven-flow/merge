e291690f87b95f892dda5403ddac262da098e85c
Jaroslav Brtis <jaroslav.brtis@gmail.com>
2024-04-28 07:37:28 -0400

    Updated pom merge driver to work with multi-module projects.

--- pom-merge-driver.sh
+++ pom-merge-driver.sh
@@ -1,29 +1,91 @@
 #!/bin/bash
 
+getVersion() {
+	file=$1
+
+	version=$(xmlstarlet sel -t -m _:project -v _:version "${file}")
+
+	if [ -z "$version" ]; then
+		version=$(xmlstarlet sel -t -m _:project/_:parent -v _:version "${file}")
+	fi
+
+	echo "$version"
+}
+
+setVersion() {
+
+	file=$1
+	oldVersion=$2
+	newVersion=$3
+
+	tempFile=$(mktemp)
+
+	insideDependency=false
+
+	# Loop through each line in the input file
+	while IFS= read -r line; do
+
+		if [[ $line == *"<dependency>"* ]]; then
+			insideDependency=true
+		fi
+
+		if [[ $line == *"</dependency>"* ]]; then
+			insideDependency=false
+		fi
+
+		if [[ $insideDependency == false ]]; then
+			# Check if line contains the old version and adjust if necessary
+			if [[ $line == *"<version>${oldVersion}</version>"* ]]; then
+				# Replace the version
+				line=$(echo "$line" | sed "s/<version>${oldVersion}<\/version>/<version>${newVersion}<\/version>/")
+			fi
+		fi
+
+		# Append the modified line to the output file
+		echo "$line" >> $tempFile
+
+	done < $file
+
+	mv $tempFile $file
+}
+
+# ----------------------- start -----------------------------
+
 ours=$1
 base=$2
 theirs=$3
 
-ourVersion=$(mvn -B help:evaluate -Dexpression=project.version -q -DforceStdout --file ${ours})
+if ! command -v xmlstarlet &> /dev/null; then
+    echo "xmlstarlet is not installed. Installing it now..."
+    sudo apt -y install xmlstarlet
+fi
+
+ourVersion=$(getVersion $ours)
 echo "Our version: ${ourVersion}"
 
-baseVersion=$(mvn -B help:evaluate -Dexpression=project.version -q -DforceStdout --file ${base})
+baseVersion=$(getVersion $base)
 echo "Base version: ${baseVersion}"
 
-theirVersion=$(mvn -B help:evaluate -Dexpression=project.version -q -DforceStdout --file ${theirs})
+theirVersion=$(getVersion $theirs)
 echo "Their version: ${theirVersion}"
 
 if [ "$ourVersion" != "$baseVersion" ] && [ "$theirVersion" != "$baseVersion" ] && [ "$ourVersion" != "$theirVersion" ]; then
     echo "Found a version conflict."
     foundVersionConflict=true
-    mvn versions:set -DnewVersion=${theirVersion} -DgenerateBackupPoms=false --file ${ours}
+    setVersion $ours $ourVersion $theirVersion
 else
     echo "No version conflict found."
     foundVersionConflict=false
 fi
 
 git merge-file $ours $base $theirs
+exitCode=$?
+
+if [ $exitCode -ne 0 ]; then
+    echo "Error: Command 'git merge file' failed with exit code $exitCode. Exiting."
+    exit 1
+fi
 
 if [ "$foundVersionConflict" = "true" ]; then
-    mvn versions:set -DnewVersion=${ourVersion} -DgenerateBackupPoms=false --file ${ours}
+    setVersion $ours $theirVersion $ourVersion
 fi
